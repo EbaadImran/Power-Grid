3e6b60474cb993178d21a4812a087e7c9d3aaf5c
dabes <dabes@10.65.18.246>
2020-02-27 15:14:11 -0600

    Deck

--- src/Deck.java
+++ src/Deck.java
@@ -1,8 +1,19 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 public class Deck {
 	private ArrayList<Card> deck;
-	public Deck() {
+
+	public Deck() throws IOException {
+		BufferedReader f = new BufferedReader(new FileReader("cityInfo.txt"));
 		deck = new ArrayList<>();
+		for (int i = 0; i < 42; i++) {
+			StringTokenizer temp = new StringTokenizer(f.readLine());
+			deck.add(new Card(Integer.parseInt(temp.nextToken()), Integer.parseInt(temp.nextToken()),
+					Integer.parseInt(temp.nextToken()), Integer.parseInt(temp.nextToken())));
+		}
 	}
 }
